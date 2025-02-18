50b3a9abba0dcd5ec7eb7d8a57da24f93d5b10a6
Elvis Kerebi <kerebipreye@gmail.com>
2025-02-18 18:40:06 +0200

    updated script in web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,6 +2,7 @@
 """Script that starts a Flask web application"""
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
@@ -10,4 +11,4 @@ def hello_hbnb():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000)
+    app.run(host='0.0.0.0', port=5000)
