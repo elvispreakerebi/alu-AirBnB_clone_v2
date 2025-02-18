69d0b0249ee16507770cf3d6a9f2c84ec3197cd1
Elvis Kerebi <kerebipreye@gmail.com>
2025-02-18 19:15:25 +0200

    updated file storage, db storage, and state files

--- models/state.py
+++ models/state.py
@@ -3,6 +3,8 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+import models
+from models.city import City
 
 class State(BaseModel, Base):
     """ State class for HBNB project """
@@ -12,3 +14,14 @@ class State(BaseModel, Base):
 
     # Relationship with City
     cities = relationship("City", back_populates="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """Returns the list of City objects from storage linked to the current State"""
+        if models.storage_type != "db":
+            city_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+        return self.cities
